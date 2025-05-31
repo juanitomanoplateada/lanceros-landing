3343d7909c4cc0b3dff67a1595ae81685001d65e
EdwinNino-24 <edwinnino2404@gmail.com>
2025-05-31 14:43:27 -0500

    v1.5

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
+import { provideServerRendering } from '@angular/platform-server';
 
-const bootstrap = () => bootstrapApplication(App, config);
+const bootstrap = () =>
+  bootstrapApplication(App, {
+    ...config,
+    providers: [
+      ...(config.providers || []),
+      provideServerRendering(),
+      {
+        provide: 'STREAM_URL',
+        useValue: process.env['STREAM_URL'],
+      },
+    ],
+  });
 
 export default bootstrap;
